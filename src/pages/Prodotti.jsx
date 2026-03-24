6aa63606463b7fbc9c67adaf32e50f776fe067b0
SimoneZoroddu <lenviur@gmail.com>
2026-03-24 15:09:55 +0100

    milestone 3 with checkbox that check if show one or other

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,14 +1,27 @@
 import { useState, useEffect } from "react"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from "axios";
+
+
+import { useContext } from "react"
+import BudgetContext from "../contexts/BudgetContext"
+
 
 const urlProduct = 'https://fakestoreapi.com/products'
 
 
+
+
+
+
 export default function Prodotti() {
+
+    const { budgetMode } = useContext(BudgetContext)
+
     const [productArray, setProductArray] = useState([])
 
-
+    const arrayWithLessThan30 = productArray.filter(singleProduct => singleProduct.price <= 30)
+    console.log(arrayWithLessThan30);
 
 
     useEffect(() => {
@@ -30,19 +43,35 @@ export default function Prodotti() {
                 <div className="container bg_confident">
                     <div className="row row-cols-3">
                         {
-                            productArray.map(singleProduct => (
-                                <div className="col g-4 text-center" key={singleProduct.id}>
-                                    <div className="card bg_confident border-0 h-100 position-relative text-white">
-                                        <h5 className="card-title text_funny fs-2" style={{height: "7rem"}} >{singleProduct.title}</h5>
-                                        <img src={singleProduct.image} className="card-img-top bg_funny" alt={singleProduct.title}  style={{height: "30rem"}}/>
-                                        <div className="card-body">
-                                            <p className="card-text text-primary text-start">{singleProduct.price} €</p>
-                                            <Link to={`/prodotti/${singleProduct.id}`}>Dettagli Prodotto</Link>
-                                            <div className="position-absolute bottom-0 end-0">Voto:{singleProduct.rating.rate} Recensioni: {singleProduct.rating.count}</div>
+                            budgetMode
+                                ?
+                                arrayWithLessThan30.map(singleProduct => (
+                                    <div className="col g-4 text-center" key={singleProduct.id}>
+                                        <div className="card bg_confident border-0 h-100 position-relative text-white">
+                                            <h5 className="card-title text_funny fs-2" style={{ height: "7rem" }} >{singleProduct.title}</h5>
+                                            <img src={singleProduct.image} className="card-img-top bg_funny" alt={singleProduct.title} style={{ height: "30rem" }} />
+                                            <div className="card-body">
+                                                <p className="card-text text-primary text-start">{singleProduct.price} €</p>
+                                                <Link to={`/prodotti/${singleProduct.id}`}>Dettagli Prodotto</Link>
+                                                <div className="position-absolute bottom-0 end-0">Voto:{singleProduct.rating.rate} Recensioni: {singleProduct.rating.count}</div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))
+                                ))
+                                :
+                                productArray.map(singleProduct => (
+                                    <div className="col g-4 text-center" key={singleProduct.id}>
+                                        <div className="card bg_confident border-0 h-100 position-relative text-white">
+                                            <h5 className="card-title text_funny fs-2" style={{ height: "7rem" }} >{singleProduct.title}</h5>
+                                            <img src={singleProduct.image} className="card-img-top bg_funny" alt={singleProduct.title} style={{ height: "30rem" }} />
+                                            <div className="card-body">
+                                                <p className="card-text text-primary text-start">{singleProduct.price} €</p>
+                                                <Link to={`/prodotti/${singleProduct.id}`}>Dettagli Prodotto</Link>
+                                                <div className="position-absolute bottom-0 end-0">Voto:{singleProduct.rating.rate} Recensioni: {singleProduct.rating.count}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
                         }
                     </div>
                 </div>
