b734529cd7b3feab75f13c912e1461ed7c921ee1
SimoneZoroddu <lenviur@gmail.com>
2026-03-24 17:31:51 +0100

    return the site without BudgetContext

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import AppHeader from "../components/AppHeader";
 import AppFooter from "../components/AppFooter";
-import Navbar from "../components/Navbar";
 
 export default function DefaultLayout() {
 
@@ -10,7 +9,6 @@ export default function DefaultLayout() {
 
     return (
         <>
-            <Navbar />
             <AppHeader />
             <Outlet />
             <AppFooter />
--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -3,9 +3,6 @@ import { Link } from 'react-router-dom'
 import axios from "axios";
 
 
-import { useContext } from "react"
-import BudgetContext from "../contexts/BudgetContext"
-
 
 const urlProduct = 'https://fakestoreapi.com/products'
 
@@ -16,12 +13,8 @@ const urlProduct = 'https://fakestoreapi.com/products'
 
 export default function Prodotti() {
 
-    const { budgetMode } = useContext(BudgetContext)
 
     const [productArray, setProductArray] = useState([])
-
-    const arrayWithLessThan30 = productArray.filter(singleProduct => singleProduct.price <= 30)
-    console.log(arrayWithLessThan30);
 
 
     useEffect(() => {
@@ -42,23 +35,7 @@ export default function Prodotti() {
             <main className="bg_main pb-4">
                 <div className="container bg_confident">
                     <div className="row row-cols-3">
-                        {
-                            budgetMode
-                                ?
-                                arrayWithLessThan30.map(singleProduct => (
-                                    <div className="col g-4 text-center" key={singleProduct.id}>
-                                        <div className="card bg_confident border-0 h-100 position-relative text-white">
-                                            <h5 className="card-title text_funny fs-2" style={{ height: "7rem" }} >{singleProduct.title}</h5>
-                                            <img src={singleProduct.image} className="card-img-top bg_funny" alt={singleProduct.title} style={{ height: "30rem" }} />
-                                            <div className="card-body">
-                                                <p className="card-text text-primary text-start">{singleProduct.price} €</p>
-                                                <Link to={`/prodotti/${singleProduct.id}`}>Dettagli Prodotto</Link>
-                                                <div className="position-absolute bottom-0 end-0">Voto:{singleProduct.rating.rate} Recensioni: {singleProduct.rating.count}</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))
-                                :
+                                {
                                 productArray.map(singleProduct => (
                                     <div className="col g-4 text-center" key={singleProduct.id}>
                                         <div className="card bg_confident border-0 h-100 position-relative text-white">
@@ -72,7 +49,7 @@ export default function Prodotti() {
                                         </div>
                                     </div>
                                 ))
-                        }
+                                }
                     </div>
                 </div>
             </main>
