099ad4fd23f0e7696182d63cba92c775e6c6ff38
SimoneZoroddu <lenviur@gmail.com>
2026-03-24 16:05:36 +0100

    adjust various

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { useState } from 'react'
-
-
 import DefaultLayout from "./layout/DefaultLayout"
 import HomePage from "./pages/HomePage"
 import Prodotti from "./pages/Prodotti"
 import ChiSiamo from "./pages/ChiSiamo"
 import SingleProduct from "./pages/SingleProduct"
-import BudgetContext from './contexts/BudgetContext'
-
 
 function App() {
 
 
-  const [budgetMode, setBudgetMode] = useState(false)
-
-
   return (
     <>
-      <BudgetContext.Provider value={{budgetMode, setBudgetMode}} >
-        <BrowserRouter>
-          <Routes>
-            <Route element={<DefaultLayout />} >
-              <Route index element={<HomePage />} />
-              <Route path="/prodotti" element={<Prodotti />} />
-              <Route path="/chisiamo" element={<ChiSiamo />} />
-              <Route path="/prodotti/:id" element={<SingleProduct />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </BudgetContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />} >
+            <Route index element={<HomePage />} />
+            <Route path="/prodotti" element={<Prodotti />} />
+            <Route path="/chisiamo" element={<ChiSiamo />} />
+            <Route path="/prodotti/:id" element={<SingleProduct />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
